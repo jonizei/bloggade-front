3bc961a1a373d0468082a340d6fe0bed4627a309
jonizei <joni.koskinen@tuni.fi>
2019-03-12 13:21:11 +0200

    blog post visualisation

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -6,7 +6,16 @@ class BlogPost extends Component {
     render() {
         return(
             <div className="blog-post">
-                <div className="blog-header"></div>
+                <div className="blog-header">
+                    <div className="blog-title blog-text">Title 1</div>
+                    <div className="blog-author blog-text">Author 1</div>
+                </div>
+                <div className="blog-content">
+                    <div className="blog-description blog-text">Description</div>
+                </div>
+                <div className="blog-footer">
+                    <div className="blog-comments blog-text">Comments</div>
+                </div>
             </div>
         );
     }
