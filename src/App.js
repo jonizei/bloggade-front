436a78fdca3339a31ba0b4b99744c6fd85dc3f69
jonizei <joni.koskinen@tuni.fi>
2019-03-18 11:10:13 +0200

    Made front page individual component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import BlogList from './BlogList.js';
-import BlogArticle from './BlogArticle.js';
+import FrontPage from './FrontPage.js';
 
 class App extends Component {
 
-  state = {mode : 'browse'};
-
   render() {
-    let printObj = null;
-
-    if(this.state.mode === 'read') {
-      printObj = <BlogArticle />;
-    } else if(this.state.mode === 'browse') {
-      printObj = <BlogList />
-    }
-
     return (
       <div className="App">
-        <div className="left-sidebar"></div>
-        {printObj}
-        <div className="right-sidebar"></div>
+        <FrontPage />
       </div>
     );
   }
---
+++ src/FrontPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './FrontPage.css';
+import BlogList from './BlogList.js';
+import BlogArticle from './BlogArticle.js';
+
+class FrontPage extends Component {
+
+    state = {mode : 'browse'};
+
+    render() {
+        let printObj = null;
+
+        if(this.state.mode === 'read') {
+        printObj = <BlogArticle />;
+        } else if(this.state.mode === 'browse') {
+        printObj = <BlogList />
+        }
+
+        return (
+        <div className="page-container">
+            <div className="left-sidebar"></div>
+            {printObj}
+            <div className="right-sidebar"></div>
+        </div>
+        );
+    }
+
+}
+
+export default FrontPage;
