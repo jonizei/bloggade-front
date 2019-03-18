2237ff5b7ed76c539762b64b8ce3722511a1c26c
jonizei <joni.koskinen@tuni.fi>
2019-03-18 12:39:58 +0200

    Modified Login UI

--- src/Login.js
+++ src/Login.js
@@ -7,16 +7,23 @@ class Login extends Component {
         return(
             <div className="page-container">
                 <div className="login-dialog">
-                    <div className="input-container">
-                        <div className="username-input">
-                            <label className="input-label">Username:</label>
-                            <input type="text" name="username" />
-                        </div>
 
-                        <div className="password-input">
-                            <label className="input-label">Password:</label>
-                            <input type="password" name="password" />
-                        </div>
+                    <div className="login-header">
+                        <h1>Login</h1>
+                    </div>
+
+                    <div className="input-container">
+
+                        <table className="input-grid">
+                            <tr className="input-row">
+                                <td><label className="input-label">Username:</label></td>
+                                <td><input className="input-field" type="text" name="username" /></td>
+                            </tr>
+                            <tr className="input-row">
+                                <td><label className="input-label">Password:</label></td>
+                                <td><input className="input-field" type="password" name="password" /></td>
+                            </tr>
+                        </table>
 
                         <div className="submit-input">
                             <input className="login-button" type="submit" value="Login" />
