4f61b715398afa958dcfe5bb2a799d6da8320eb3
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-04-17 14:52:55 +0300

    add delete comment

--- src/BlogArticle.js
+++ src/BlogArticle.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import './BlogArticle.css';
 
 class Comments extends Component {
-  render() {
-    console.log('Comments');
-    let comments = this.props.comments
-    let divComments = comments.map((comment) => {
-      return <div className="blog-article-content blog-text">{comment.text}</div>
-    })
-    return <div>{divComments}</div>
-  }
+    constructor(props) {
+        super(props);
+
+        this.onDeleteClick = this.onDeleteClick.bind(this);
+        this.deleteComment = this.deleteComment.bind(this);
+
+        this.state = {
+            comments: this.props.comments,
+            updatePosts: this.props.updatePosts
+        }
+    }
+
+    onDeleteClick = event => {
+        event.preventDefault();
+        this.deleteComment(event.target.id);
+    }
+
+    deleteComment(id) {
+        fetch('http://localhost:8080/api/private/admin/delete/comment/' + id, {
+            method: 'DELETE'
+        }).then((httpResp) => {
+            this.state.updatePosts();
+        }).catch(this.onError);
+    }
+
+    onError(msg) {
+        console.log(msg);
+    }
+
+    render() {
+        console.log('Comments');
+        let comments = this.state.comments
+        let divComments = comments.map((comment) => {
+        return  <div className="blog-article-content blog-text">{comment.text}
+                    <div className="blog-delete" onClick={this.onDeleteClick} id={comment.id} >DEL</div>
+                </div>
+        })
+        return <div>{divComments}</div>
+    }
 }
 
 class BlogArticle extends Component {
@@ -261,7 +292,7 @@ class BlogArticle extends Component {
                   {addComment}
                 </div>
                 <div>
-                  <Comments comments={tempComments}/>
+                  <Comments comments={tempComments} updatePosts={this.state.updatePosts} />
                 </div>
               </div>
             );
